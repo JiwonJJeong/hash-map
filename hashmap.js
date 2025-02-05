d75f5c1a1b7dd4218a62a6942875f3cd317cd996
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-02-05 15:37:18 -0800

    Create hash map class

--- hashmap.js
+++ hashmap.js
@@ -1,6 +1,16 @@
 
 
+class HashMap {
+  static LOAD_FACTOR = 0.9;
+  capacity =16;
+
 // overrides JavaScript's dynamic increasing of array size
-if (index < 0 || index >= buckets.length) {
+// should be used whenever access bucket through index
+ checkIndex(index){
+  if (index < 0 || index >= buckets.length) {
     throw new Error("Trying to access index out of bounds");
   }
+}
+
+}
+
