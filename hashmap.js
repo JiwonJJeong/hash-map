8c8848b621b8ce6e8fde27625e75fa8bb45e6a26
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-02-05 15:40:51 -0800

    Add hash function

--- hashmap.js
+++ hashmap.js
@@ -12,5 +12,18 @@ class HashMap {
   }
 }
 
+// only takes strings as keys
+// takes modulo to prevent integer overflow AND limits hashCode to bucket length
+  hash(key){
+    let hashCode = 0;
+      
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i))%capacity;
+    }
+ 
+    return hashCode;
+  }
+
 }
 
